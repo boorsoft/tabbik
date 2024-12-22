de2e4983fdc0486a6f3331a66f48e1898984da53
akmatoff <akmatoff.exe@gmail.com>
2024-12-23 05:11:12 +0600

    add user role

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,5 +1,13 @@
 import { InferSelectModel } from "drizzle-orm";
-import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+import {
+  pgEnum,
+  pgTable,
+  serial,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const userRole = pgEnum("userRole", ["ADMIN", "USER"]);
 
 export const user = pgTable("user", {
   id: serial("id").primaryKey(),
@@ -9,8 +17,9 @@ export const user = pgTable("user", {
   avatar: varchar("avatar", { length: 256 }),
   firstName: varchar("firstName", { length: 100 }),
   lastName: varchar("lastName", { length: 100 }),
-  createdAt: timestamp("createdAt").defaultNow(),
-  updatedAT: timestamp("updatedAt").defaultNow(),
+  role: userRole("userRole").default("USER").notNull(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+  updatedAT: timestamp("updatedAt").defaultNow().notNull(),
 });
 
 export type User = InferSelectModel<typeof user>;
