270f3e1c229d2fcad2cf9acb44b5d5bdb28b58d6
akmatoff <akmatoff.exe@gmail.com>
2025-01-01 05:59:07 +0600

    add openapi spec

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,13 +1,24 @@
 import { Router } from "express";
+import swaggerUi from "swagger-ui-express";
+
 import tournaments from "./tournaments/tournaments.routes";
 import teamInvitations from "./teamInvitations/teamInvitations.routes";
 import { authMiddleware } from "../../middleware/auth.middleware";
 import { auth } from "./auth/auth.routes";
 import tournamentTeamsRoutes from "./tournamentTeams/tournamentTeams.routes";
 import userRoutes from "./user/user.routes";
+import yaml from "js-yaml";
+import { readFileSync } from "fs";
 
 const api = Router();
 
+const apiDocs = yaml.load(readFileSync("openapi.yaml", "utf8")) as Record<
+  string,
+  unknown
+>;
+
+api.use("/docs", swaggerUi.serve);
+api.get("/docs", swaggerUi.setup(apiDocs));
 api.use("/auth", auth);
 
 api.use(authMiddleware);
